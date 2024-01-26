22b5eb4be3eb444cdf29118bb826d3a0a81d3a6a
LaecioFillho <laeciofillho@gmail.com>
2024-01-26 17:25:58 -0300

    AleTraining V0.0.2
    Adicionando novas informações

--- codigo/controle.js
+++ codigo/controle.js
@@ -1,3 +1,7 @@
+//Declaração de Variiaveis:
+
+//Variaveis do menu inicial:
+
 let btnDia = document.querySelector('.btn-dias')
 let dia = document.querySelector('.dias')
 
@@ -7,7 +11,21 @@ let treino = document.querySelector('.treinos')
 let btnTipo = document.querySelector('.btn-tipos')
 let tipo = document.querySelector('.tipos')
 
-//Ação da caixa Dias
+//Variaveis da Cx Dias:
+
+let btnSegunda = document.querySelector('.btn-segunda')
+let btnTerca = document.querySelector('.btn-terca')
+let btnQuarta = document.querySelector('.btn-quarta')
+let btnQuinta = document.querySelector('.btn-quinta')
+let btnSexta = document.querySelector('.btn-sexta')
+
+let diaSegunda = document.querySelector('.segunda')
+let diaTerca = document.querySelector('.terca')
+let diaQuarta = document.querySelector('.quarta')
+let diaQuinta = document.querySelector('.quinta')
+let diaSexta = document.querySelector('.sexta')
+
+//Ação da caixa Dias:
 
 btnDia.addEventListener('click', function(){
 
@@ -15,12 +33,14 @@ btnDia.addEventListener('click', function(){
         dia.style.display = 'block'
         treino.style.display = 'none'
         tipo.style.display = 'none'
+        diaSegunda.style.display = 'none'
     } else {
         dia.style.display = 'none'
+        diaSegunda.style.display = 'none'
     }
 })
 
-//Ação da caixa Trinos
+//Ação da caixa Trinos:
 
 btnTreino.addEventListener('click', function(){
 
@@ -28,12 +48,13 @@ btnTreino.addEventListener('click', function(){
         treino.style.display = 'block'
         tipo.style.display = 'none'
         dia.style.display = 'none'
+        diaSegunda.style.display = 'none'
     } else {
         treino.style.display = 'none'
     }
 })
 
-//Ação da caixa Tipos
+//Ação da caixa Tipos:
 
 btnTipo.addEventListener('click', function(){
 
@@ -41,7 +62,21 @@ btnTipo.addEventListener('click', function(){
         tipo.style.display = 'block'
         dia.style.display = 'none'
         treino.style.display = 'none'
+        diaSegunda.style.display = 'none'
     } else {
         tipo.style.display = 'none'
+    }
+})
+
+
+//Ação do menu Dias:
+
+btnSegunda.addEventListener('click', function(){
+
+    if (diaSegunda.style.display === 'none') {
+        diaSegunda.style.display = 'block' 
+        dia.style.display = 'none'      
+    } else {
+        diaSegunda.style.display = 'none'
     }
 })
